04dd3fd69ed5c312aad90c0b948e57af9d768ad3
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-02 17:57:56 -0800

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,7 @@
 def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            ord(c) = ord(c) - 32
-        print("{:c}".format(ord(c)), end="")
+            print("{:c}".format(ord(c) - 32), end="")
+        else:
+            print("{:c}".format(ord(c)), end="")
     print("")
