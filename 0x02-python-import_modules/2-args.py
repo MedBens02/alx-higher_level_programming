6561b311998d728caacd5139f26ce117dcf7ccca
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-04 15:50:19 -0800

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,13 +5,11 @@ if __name__ == "__main__":
     import sys
 
     nbr = len(sys.argv) - 1
-
     if nbr == 0:
         print("0 arguments.")
-    if nbr == 1:
+    elif nbr == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(nbr))
-
     for i in range(nbr):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
