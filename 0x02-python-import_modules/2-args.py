dc2e5410307b448d9d2a65113db23748ea0dc473
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-04 15:48:08 -0800

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    nbr = len(sys.argv) - 1
+
+    if nbr == 0:
+        print("0 arguments.")
+    if nbr == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(nbr))
+
+    for i in range(nbr):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
