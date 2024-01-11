0302e582ec6a6a37c88d8e405e4b59835d63539e
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-10 17:39:11 -0800

    e2

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,6 +4,6 @@ def complex_delete(a_dictionary, value):
 
     for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
-    	    del a_dictionary[key]
+            del a_dictionary[key]
 
     return a_dictionary
