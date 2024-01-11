d11531c57e317633bbf2b15cb3da311f3fa67278
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-10 17:06:16 -0800

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,16 @@
 
 def roman_to_int(roman_string):
 
-    if type(roman_string) != str or !roman_string:
+    if type(roman_string) != str:
         return 0
 
-    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000)
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
     tmp = 0
     val = 0
 
     for R in reversed(roman_string):
-        val = romans[R]
+        val = romans.get(R, 0)
 
         if val < tmp:
             result -= val
