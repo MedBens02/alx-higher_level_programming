2d2a8df4b92d928cb70f46e7313fd61f3609d834
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-22 15:07:13 -0800

    e0

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
 
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except (ValueError, TypeError) as err:
         print("Exception: {}".format(err), file=sys.stderr)
-        return False
+        return (False)
