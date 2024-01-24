e2a34fdff7a6fda8c1c93a6811c8cfb81bdd5cb4
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-23 17:01:45 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This is a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """New Square.
+
+        Args:
+            size: The size of a square.
+        """
+        self.__size = size
