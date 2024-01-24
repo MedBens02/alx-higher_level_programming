f04d9dc7dc918863a7f8cee7779543ebfbc4645f
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-23 17:39:11 -0800

    e2

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""This is a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """New Square.
+
+        Args:
+            size: The size of a square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Access the size of square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area of square"""
+        return (self.__size ** 2)
+
+     def __eq__(self, other):
+        """ == compare."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """ != compare."""
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        """ > compare."""
+        return self.area() > other.area()
+
+    def __lt__(self, other):
+        """ < compare."""
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        """ >= compmare."""
+        return self.area() >= other.area()
+
+    def __le__(self, other):
+        """ <= compare."""
+        return self.area() <= other.area()
