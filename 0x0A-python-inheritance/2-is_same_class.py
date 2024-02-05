a5afac38d5bf95f826fe3e1eaeddaff549582075
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-05 15:35:12 -0800

    2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a fct that checks if an obj is an instance"""
+'''Defines a fct that checks if an obj is an instance'''
 
 
 def is_same_class(obj, a_class):
-    """Returns True is obj is exactly an instance of a class."""
+    '''Returns True is obj is exactly an instance of a class.'''
      if type(obj) == a_class:
         return True
     return False
