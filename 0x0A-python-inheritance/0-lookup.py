8c99d7de1599c1a6f7e43dce3727626ea118035c
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-05 14:33:07 -0800

    0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a fct that lists attributes & methods of an object"""
+
+
+def lookup(obj):
+    """Returns list of att/methods"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+The 10th python project
