821f4e343f958281e26d9c58a9151f44cf97ddf4
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-05 16:30:12 -0800

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Defines a subclass of rectangle: Square'''
+Rect = = __import__('9-rectangle').Rectangle
+
+class Square(Rect):
+    '''A Square'''
+
+    def __init__(self, size):
+        """Initialize a Square.
+
+        Args:
+            size: size of square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        '''Area method'''
+        return self.__size ** 2
