1f756a6f61742bbdfe0a6d19c678369e880af3ed
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-05 16:00:14 -0800

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Empty class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''BaseGeometry class'''
+    
+    def area(self):
+        '''area method'''
+        raise Exception("area() is not implemented")
