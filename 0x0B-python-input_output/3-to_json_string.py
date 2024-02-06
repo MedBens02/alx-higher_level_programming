e7bb2e9d7ab0dc7f80086c3b223a5fe89bf9f704
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-06 02:59:30 -0800

    3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Defines a JSON representation fct'''
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of object.
+
+    Args:
+        my_obj: obj to serialize.
+    Return:
+        its JSON representation.
+    """
+    return json.dumps(my_obj)
