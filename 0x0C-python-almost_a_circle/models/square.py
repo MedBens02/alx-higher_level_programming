838dc3be6eb8e024cc33653f13eef802d23055b6
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-12 13:36:23 -0800

    14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -51,5 +51,4 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """Returns the dict representation of a square"""
-        return {'id': self.id, 'width': self.width,
-                'x': self.x, 'y': self.y}
+        return {'id': self.id, 'width': self.size, 'x': self.x, 'y': self.y}
