9e65b5181b1e3ae9d6973ac99c1ac13fe9872cd5
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-03-13 16:50:01 -0700

    13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tg.name AS 'genre', COUNT(tsg.genre_id) AS 'number_of_shows'
 FROM tv_genre tg
 RIGHT JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 GROUP BY genre
-ORDER BY number_pf_shows DESC;
+ORDER BY number_of_shows DESC;
