28094f3c9c0ea6d74fc78ead9bed59263f0f94c3
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-04-09 17:58:55 -0700

    8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let len = list.length;
+  const len = list.length;
   for (let i = 0; i < Math.floor(len / 2); i++) {
     [list[i], list[len - 1 - i]] = [list[len - 1 - i], list[i]];
   }
