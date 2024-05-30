3a35b4eef3e76e73cb7f932124c4ab1275beb2b3
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-05-29 18:58:18 -0700

    0

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector("header").style.color = "#FF0000";
-});
+document.querySelector("header").style.color = "#FF0000";
